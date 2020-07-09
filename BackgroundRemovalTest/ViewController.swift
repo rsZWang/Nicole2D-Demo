035dd83eca3fa4f4913649a6339e54b869f6b304
yhw <cool890104@yahoo.com.tw>
2020-07-09 20:55:17 +0800

    Initial Commit

---
+++ BackgroundRemovalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundRemovalTest
+//
+//  Created by Nihontabako on 2020/7/9.
+//  Copyright © 2020 YHWang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
